d8dbb2dbae701bd9cb69ff500ee3564a09c237b5
Username <tillaberdiyevibrat@gmail.com>
2022-06-24 02:47:30 +0500

    some change

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = (app) => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World !}
-    console.log(context);
     const params = context.issue({
       body: "Rahmat. Xatoni bildirganingiz uchun. Xatoni ko'rib chiqamiz va unga label belgilaymiz",
     });
