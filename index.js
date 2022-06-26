1f4d27c9b1904d8855118b10c3f328a416bde4f6
Username <tillaberdiyevibrat@gmail.com>
2022-06-26 05:54:20 +0500

    some change

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = (app) => {
   app.on('issues.opened', async (context) => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World !}
-    const params = context.issue({
-      body: "Rahmat. Xatoni bildirganingiz uchun. Xatoni ko'rib chiqamiz va unga label belgilaymiz",
-    });
+    // const params = context.issue({
+    //   body: "Rahmat. Xatoni bildirganingiz uchun. Xatoni ko'rib chiqamiz va unga label belgilaymiz",
+    // });
+
+    const result = app.auth();
+
+    console.log(result);
 
     // Post a comment on the issue
     return context.octokit.issues.createComment(params);
